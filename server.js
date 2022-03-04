18b3d4ae8c5d0d25fbf59176be4f098a19375f0c
marcos <marcoscorreats@gmail.com>
2022-03-04 01:53:16 -0300

    alterando versao estatica

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // import express
 const express = require('express');
+const express = require('express');
 // init express
 const app = express();
 // name folder dist to build
@@ -7,11 +8,10 @@ const appName = 'markdev-app';
 // local build will generate files
 const outputPath = `${__dirname}/dist/${appName}`;
 // set build directory to serve angular content
-app.use(express.json());
+app.use(express.static(outputPath));
 // redirect requisitions: 
 app.get('/*',(req,res)=>{
   res.sendFile(`${outputPath}/index.html`);
 });
 // listen heroku port
 app.listen(process.env.PORT);
-
